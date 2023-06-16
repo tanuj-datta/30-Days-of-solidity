6e962fb809f5cb2f8dccc6d29d24d7254bf8c471
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-16 08:22:33 +0530

    Add files via upload

---
+++ fristcontract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract FirstContract{
+    string public  hello = "Hello Tanuj Datta!";
+    uint256 public no = 4;
+
+    constructor(string memory _hey, uint _no){
+       hello = _hey;
+        no = _no;
+    
+    
+
+  
+
+    }
+
+
+}
