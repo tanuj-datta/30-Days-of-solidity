ae2a2a70f544daffb3efbfa416267ec262e6600b
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-19 21:50:29 +0530

    Update firstcontract.sol

--- firstcontract.sol
+++ firstcontract.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.13;
 contract FirstContract{
     string public  hello = "Hello Tanuj Datta!";
     uint256 public no = 4;
+    uint176 public number = 15;
 
-    constructor(string memory _hey, uint _no){
+    constructor(string memory _hey, uint _no, uint _number){
        hello = _hey;
         no = _no;
+        number = _number;
     
     
 
