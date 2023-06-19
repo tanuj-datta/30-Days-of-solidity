b2f9f0b769ab322b4c6fabcc63a81dd5d4b5584b
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-19 21:46:22 +0530

    Create Day4.sol

---
+++ Day4.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.13;
+
+contract DaYFour {
+    uint256[] numbers;
+
+    struct person{
+        string name;
+        uint256 age;
+    }
+
+    person[] people;
+
+    function ArraySum(uint256[] memory _numbers) public pure returns {
+        uint256 sum =0;
+        for(uint256 i=0;i<_numbers.length;i++) {
+            sum = sum+ _numbers[i];
+        }
+        return sum;
+    }
+
+     function addPerson(string memory _name, uint256 _age) public {
+        Person memory newPerson = Person(_name, _age);
+        people.push(newPerson);
+}
+
+}
