65ef03a07dcbdcbf140d01efdec9b2194f0230f6
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-20 22:23:31 +0530

    Create Day6.sol

---
+++ Day6.sol
@@ -0,0 +1,30 @@
+// mapping and modifiers
+
+pragma solidity ^0.8.13;
+
+contract MappingAndModifiers {
+    mapping(address => uint256) balances;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can access this function");
+       
+    }
+
+    modifier positiveAmount(uint256 _amount) {
+        require(_amount > 0, "Amount must be positive");
+      
+    }
+
+    function deposit(uint256 _amount) public onlyOwner positiveAmount(_amount) {
+        balances[msg.sender] += _amount;
+    }
+
+    function getBalance(address _address) public view returns (uint256) {
+        return balances[_address];
+    }
+}
