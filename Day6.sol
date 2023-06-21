45fe1074e13d68b739d5a535775d4bb984da9831
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-21 21:57:27 +0530

    Create Day6.sol

---
+++ Day6.sol
@@ -0,0 +1,21 @@
+//events and error handling in Solidity
+pragma solidity ^0.8.13;
+
+contract EventsAndErrorHandling {
+    mapping(address => uint256) balances;
+
+    event Transfer(address indexed sender, address indexed recipient, uint256 amount);
+
+    function transfer(address _recipient, uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient funds");
+        
+        balances[msg.sender] -= _amount;
+        balances[_recipient] += _amount;
+
+        emit Transfer(msg.sender, _recipient, _amount);
+    }
+
+    function getBalance(address _address) public view returns (uint256) {
+        return balances[_address];
+    }
+}
