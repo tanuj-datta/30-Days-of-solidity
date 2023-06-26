bf7f448390884a7e911587553b7d0e659622c260
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-26 15:44:48 +0530

    Create Day11.sol

---
+++ Day11.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+    event VoteRecorded(address indexed voter, string candidate);
+
+    mapping(string => uint256) public candidates;
+
+    constructor() {
+        candidates["Candidate 1"] = 0;
+        candidates["Candidate 2"] = 0;
+    }
+
+    function vote(string memory _candidate) public {
+        candidates[_candidate]++;
+        emit VoteRecorded(msg.sender, _candidate);
+    }
+}
