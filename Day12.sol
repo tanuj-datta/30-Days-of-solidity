d7b6b2e424a79fa01012d7c7f3d778630717302c
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-27 23:30:49 +0530

    Create Day12.sol

---
+++ Day12.sol
@@ -0,0 +1,27 @@
+
+pragma solidity ^0.8.13;
+
+abstract contract Animal {
+    function sound() public pure virtual returns (string memory);
+    function eat(string memory food) internal virtual;
+}
+
+contract Cat is Animal {
+    function sound() public pure override returns (string memory) {
+        return "Meow";
+    }
+
+    function eat(string memory food) internal override {
+       
+    }
+}
+
+contract Dog is Animal {
+    function sound() public pure override returns (string memory) {
+        return "Woof";
+    }
+
+    function eat(string memory food) internal override {
+        
+    }
+}
