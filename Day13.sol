d42bc0c358a3295414c202470fa4ad5d49b96436
tanuj-datta <86672745+tanuj-datta@users.noreply.github.com>
2023-06-28 21:51:49 +0530

    Create Day13.sol

---
+++ Day13.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+contract Token is IERC20 {
+   
+}
+
+contract TokenHolder {
+    IERC20 private token;
+
+    function setTokenAddress(address _tokenAddress) public {
+        token = IERC20(_tokenAddress);
+    }
+
+    function transferTokens(address _recipient, uint256 _amount) public {
+        token.transfer(_recipient, _amount);
+    }
+}
